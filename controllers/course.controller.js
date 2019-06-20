d9ff9e39126d3d58a7940b7dfcb7f24025a4da65
James Ford <ozerr.murat@hotmail.com>
2019-06-20 17:58:18 +0300

    function convert async

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -3,6 +3,7 @@ const express       =       require('express');
 const router        =       express.Router();
 
 
+/** Old
 const getCourses = (req, res) => {
   return Course
   .findAll({
@@ -15,7 +16,18 @@ const getCourses = (req, res) => {
     return res.status(400).send(error)
   })
 }
+**/
 
+//New
+async function getCourses (req, res) {
+  let course = await Course.findAll({
+    attributes: ['course_id','course_name','createdAt','updatedAt'],
+    order: [['createdAt', 'DESC']]
+  });
+  return res.status(200).send(course);
+}
+
+/* Old
 const getCourseById = (req, res) => {
   return Course
   .findOne(
@@ -35,8 +47,26 @@ const getCourseById = (req, res) => {
     return res.status(400).send(error)
   })
 }
+*/
+
+//New
+async function getCourseById  (req, res) {
+  let course = await Course.findOne(
+    {
+      attributes: ['course_id','course_name','createdAt','updatedAt'],
+      limit : 1,
+      where : {
+        course_id : req.params.id
+      }
+    });
+    if (!course) {
+      return res.status(400).send('There is not such a course!');
+    }
+    return res.status(200).send(course);
+}
 
 
+/* Old
 const createCourse = (req, res) => {
   return Course
   .create({
@@ -46,8 +76,17 @@ const createCourse = (req, res) => {
   .send({"message":`Course created with course_name: ${req.body.course_name}`,"data":course}))
   .catch((error) => res.status(400).send(error));
 }
+*/
 
+// New Create
+async function createCourse (req, res) {
+  let course = await Course.create({
+    course_name: req.body.course_name
+  })
+  return res.status(200).send({"message":`Course created with course_name: ${req.body.course_name}`,"data":course});
+}
 
+/* Old
 const updateCourse = (req,res) => {
   return Course
   .findOne({
@@ -76,8 +115,30 @@ const updateCourse = (req,res) => {
     return res.status(400).send(error)
   });
 }
+*/
+
+//New Update
+async function updateCourse (req,res) {
+  let course = await Course.findOne(
+    {
+      where : {
+        course_id : req.params.id
+      }
+    }
+  )
+  if (!course) {
+    return res.status(404).send({
+      message: 'Course Not Found',
+    });
+  }
+  course.update({
+    course_name : req.body.course_name
+  })
+  return res.status(200).send({"message":`Course updated with course_id: ${req.params.id}`,"data":course})
+}
 
 
+/*
 const deleteCourse = (req,res) => {
   return Course
   .findOne({
@@ -103,6 +164,23 @@ const deleteCourse = (req,res) => {
 .catch((error) => {
   return res.status(400).send(error)
 });
+}
+*/
+
+//New Delete
+async function deleteCourse (req,res) {
+  let course = await Course.findOne({
+    where : {
+      course_id : req.params.id
+    }
+  })
+
+  if (!course) {
+    return res.status(400).send({
+      message: 'Course Not Found'
+    });
+  }
+  return res.status(200).send({"message":`Course deleted with course_id: ${req.params.id}`})
 }
 
 router.get('/', (req, res) => {res.send('Hello World!!')})
